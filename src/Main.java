e7f1b19622988773599e1b835603f021c4f9a175
phistr <strassenbergen@outlook.com>
2016-05-12 09:59:29 +0200

    Corrected file name from Draw to Motor

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ import javafx.application.Application;
 
 public class Main {
     public static void main(String[] args) {
-        Application.launch(Draw.class, args);
+        Application.launch(Motor.class, args);
     }
 }
