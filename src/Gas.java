add101a914fc6d878096266184c89c40bab0244b
phistr <strassenbergen@outlook.com>
2016-05-12 11:05:15 +0200

    Add Gas, for gas cans on the road
    Special class for gas cans on the road

---
+++ src/Gas.java
@@ -0,0 +1,21 @@
+import javafx.scene.image.Image;
+
+import java.util.Random;
+
+/**
+ * @author Philip Strömberg
+ * Special class for gas cans on the road
+ */
+public class Gas extends Entity {
+    private static Random r = new Random();
+
+    public Gas(Image img, int width, int height, GameCanvas canvas) {
+        super(img, width, height, canvas);
+        x = r.nextInt((int) canvas.getWidth());
+        y = 0;
+        ySpeed = 1 + r.nextInt(4);
+    }
+
+
+
+}
