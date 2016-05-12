e41e2620dc81b4fb13bad76365fbbb103ff29f9e
phistr <strassenbergen@outlook.com>
2016-05-12 11:02:18 +0200

    Add Obstacle, for meeting cars
    Extended class from Entity for meeting cars

---
+++ src/Obstacle.java
@@ -0,0 +1,17 @@
+import javafx.scene.image.Image;
+import java.util.Random;
+
+/**
+ * @author Philip Strömberg
+ * Extended class from Entity for meeting cars
+ */
+public class Obstacle extends Entity {
+    private static Random r = new Random();
+
+    public Obstacle(Image img, int width, int height, GameCanvas canvas) {
+        super(img, width, height, canvas);
+        x = r.nextInt((int) canvas.getWidth());
+        y = 0;
+        ySpeed = 1 + r.nextInt(4);
+    }
+}
